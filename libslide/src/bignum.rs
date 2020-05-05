27be90c4b1223de400eb8eec4b07369428a580cd
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2020-05-05 10:06:21 -0700

    Add BigNum and add operation

---
+++ libslide/src/bignum.rs
@@ -0,0 +1,114 @@
+use std::fmt;
+mod add;
+
+// this probably can go in utils but I put it in here for now
+trait StringUtils {
+    fn substring(&self, start: usize, len: usize) -> Self;
+}
+
+impl StringUtils for String {
+    fn substring(&self, start: usize, len: usize) -> Self {
+        self.chars().skip(start).take(len).collect()
+    }
+}
+
+fn to_u8(c: char) -> u8 {
+    c.to_digit(10).unwrap() as u8
+}
+
+fn to_char(n: u8) -> char {
+    std::char::from_digit(n as u32, 10).unwrap()
+}
+
+pub struct Bignum {
+    is_neg: bool,
+    // we use vector since it will allow faster computation time
+    // for smaller numbers
+    int: Vec<u8>,
+    dec: Vec<u8>,
+}
+
+impl Bignum {
+    pub fn new(input: String) -> Bignum {
+        let mut chars = input.chars();
+        let mut is_neg: bool = false;
+        let mut has_decimal = false;
+        let mut int = Vec::new();
+        let mut dec = Vec::new();
+        match chars.next() {
+            Some('-') => is_neg = true,
+            Some('.') => has_decimal = true,
+            Some(x) => int.push(to_u8(x)),
+            _ => (),
+        };
+        for c in chars {
+            if c.is_digit(10) {
+                if has_decimal {
+                    dec.push(to_u8(c));
+                } else {
+                    int.push(to_u8(c));
+                }
+            } else if c == '.' {
+                has_decimal = !has_decimal;
+                if !has_decimal {
+                    panic!("Number cannot have two decimal values");
+                }
+            } else {
+                panic!("Cannot parse string as number");
+            }
+        }
+        Bignum {
+            is_neg,
+            int: int.iter().rev().cloned().collect(),
+            dec,
+        }
+    }
+}
+
+impl fmt::Display for Bignum {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut result = String::new();
+        if self.is_neg {
+            result.push('-');
+        }
+        for n in self.int.iter().rev() {
+            result.push(to_char(*n));
+        }
+        if !self.dec.is_empty() {
+            result.push('.');
+            for n in self.dec.iter() {
+                result.push(to_char(*n));
+            }
+        }
+        write!(f, "{}", result)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    macro_rules! bignum_test {
+        ($($name:ident: $program:expr)*) => {
+        $(
+            #[test]
+            fn $name() {
+                use crate::bignum::Bignum;
+                let result = $program.to_string();
+                let bg = Bignum::new(result.clone());
+                assert_eq!(bg.to_string(), result);
+            }
+        )*
+        }
+    }
+    mod num {
+        bignum_test! {
+            int: "5"
+            negative_int: "-5"
+            float: "5.5"
+            negative_float: "-5.5"
+            bigger_int: "1002039444884993020"
+            bigger_float: "102399959939999.393993"
+            decimal: ".3"
+            negative_decimal: "-.3"
+        }
+    }
+}
---
+++ libslide/src/bignum/add.rs
@@ -0,0 +1,119 @@
+#![allow(clippy::suspicious_arithmetic_impl)]
+use crate::bignum::Bignum;
+use std::mem;
+use std::ops;
+
+impl ops::Add for Bignum {
+    type Output = Bignum;
+
+    fn add(self, rhs: Bignum) -> Bignum {
+        let is_neg: bool = rhs.is_neg && self.is_neg;
+
+        // this assumes the signs are both positive for now
+        let mut carry: u8 = 0;
+        let mut lhs_size: usize = self.dec.len();
+        let mut rhs_size: usize = rhs.dec.len();
+        let mut lhs_vec: Vec<u8>;
+
+        // make lhs smaller vector
+        // lhs_size = self, rhs_self = rhs
+        let mut res_dec = if lhs_size > rhs_size {
+            mem::swap(&mut lhs_size, &mut rhs_size);
+            lhs_vec = rhs.dec;
+            self.dec
+        } else {
+            lhs_vec = self.dec;
+            rhs.dec
+        };
+
+        for i in (0..lhs_size).rev() {
+            res_dec[i] += lhs_vec[i] + carry;
+            carry = (res_dec[i] > 9) as u8;
+            res_dec[i] %= 10;
+        }
+
+        // 2. Handle Integer
+
+        lhs_size = self.int.len();
+        rhs_size = rhs.int.len();
+
+        // make lhs smaller vector
+        let mut res_int = if lhs_size > rhs_size {
+            mem::swap(&mut lhs_size, &mut rhs_size);
+            lhs_vec = rhs.int;
+            self.int
+        } else {
+            lhs_vec = self.int;
+            rhs.int
+        };
+
+        // compute addition with both ints
+        for i in 0..lhs_size {
+            res_int[i] += lhs_vec[i] + carry;
+            carry = (res_int[i] > 9) as u8;
+            res_int[i] %= 10;
+        }
+
+        // fill with rest of the larger integer vector
+        // note we need to propogate carry here while in decimal we do not, but if carry = 0 we are
+        // done since res_int already contains the values
+        let mut i = lhs_size;
+        while carry != 0 && i < rhs_size {
+            res_int[i] += carry;
+            carry = (res_int[i] > 9) as u8;
+            res_int[i] %= 10;
+            i += 1;
+        }
+
+        // add 1 if a carry is leftover
+        if carry == 1 {
+            res_int.push(1);
+        }
+
+        Bignum {
+            is_neg,
+            int: res_int,
+            dec: res_dec,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    macro_rules! bignum_test_add {
+        ($($name: ident: $lhs:expr, $rhs:expr, $program:expr)*) => {
+        $(
+            #[test]
+            fn $name() {
+                use crate::bignum::Bignum;
+                let lhs = Bignum::new($lhs.to_string());
+                let rhs = Bignum::new($rhs.to_string());
+                let result = $program.to_string();
+                assert_eq!((lhs+rhs).to_string(), result);
+            }
+        )*
+        }
+    }
+
+    mod add {
+        bignum_test_add! {
+            int1: "5", "5", "10"
+            int2: "55", "13", "68"
+            int3: "5", "15", "20"
+            int4: "55555555555555555555555", "5555555555555555", "55555561111111111111110"
+            int5: "99999999999999999999999999999999" , "999999999999999999999", "100000000000999999999999999999998"
+            int6: "111", "1", "112"
+            float: "0.1", "0.1", "0.2"
+            float2: "0.111", "0.1", "0.211"
+            float3: "0.1", "0.111", "0.211"
+            float4: "0.5", "0.9", "1.4"
+            float5: "0.55555", "0.99", "1.54555"
+            float6: "0.555555555555555555555555555", "0.555555555555555555", "1.111111111111111110555555555"
+            float7: "0.99999999999999999", "0.999999999999999999999999999999", "1.999999999999999989999999999999"
+            float8: "0.1112", "0.923", "1.0342"
+            mixed: "1.1", "1.1", "2.2"
+            mixed2: "1.9999999", "9999.99999", "10001.9999899"
+            mixed3: "99999999999999.999999999999", "99999999999.99999", "100099999999999.999989999999"
+        }
+    }
+}

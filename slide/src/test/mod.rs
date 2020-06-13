8fcf65da94094c6e41ca4076b18486940c205794
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2020-06-12 18:53:41 -0700

    Add test cases from partial_evaluator
    This is a copy of partial_evaluator tests for output and s-expression
    cases.

--- slide/src/test/mod.rs
+++ slide/src/test/mod.rs
@@ -1,5 +1,6 @@
 use difference::{Changeset, Difference};
 use libtest_mimic::{run_tests, Arguments, LineFormat, LinePrinter, Outcome, Test};
+use std::collections::HashMap;
 use std::env;
 use std::error::Error;
 use std::ffi::OsStr;
@@ -41,10 +42,14 @@ fn collect_tests() -> Result<Vec<Test<PathBuf>>, Box<dyn Error>> {
 
 /// Describes a test case
 struct TestCase {
+    /// Annotation name -> Annotation message
+    annotations: HashMap<String, String>,
+    annotation_order: Vec<String>,
     args: String,
     input: String,
     stdout: String,
     stderr: String,
+    exitcode: String,
 }
 
 /// Returns the delimiter for a test case clause in a .slide test file.
@@ -52,18 +57,48 @@ fn get_clause_delim(clause: &str) -> String {
     let prefix = match clause {
         "args" => "!!!",
         "in" => "===",
-        "stdout" | "stderr" => "~~~",
+        "exitcode" | "stdout" | "stderr" => "~~~",
         _ => unreachable!(),
     };
     format!("{}{}", prefix, clause)
 }
 
+/// Returns whether a clause can be auto-generated with --bless.
+fn can_be_blessed(clause: &str) -> bool {
+    match clause {
+        "exitcode" | "stdout" | "stderr" => true,
+        _ => false,
+    }
+}
+
+/// Returns the command to bless a test file.
+fn get_bless_cmd(test_name: &str) -> String {
+    format!("ladder test {} --bless", test_name)
+}
+
 /// Creates a TestCase from a .slide test file.
-fn mk_test_case(mut content: String, bless: bool) -> Result<TestCase, Outcome> {
-    let clause_names = ["args", "in", "stdout", "stderr"];
+fn mk_test_case(test_name: &str, mut content: String, bless: bool) -> Result<TestCase, Outcome> {
+    // First we need to parse out annotations at the top of the file.
+    let mut annotations = HashMap::<String, String>::new();
+    let mut annotation_order = Vec::new();
+    while content.starts_with('@') {
+        let line_split = content.find('\n').unwrap();
+        let rest_content = content.split_off(line_split);
+        let annotation = content;
+        content = rest_content;
+        let mut annotation_parts = annotation.split(':');
+        let annotation = annotation_parts.next().unwrap();
+        let annotation_msg = annotation_parts.next().map(|s| s.trim()).unwrap_or("");
+        annotations.insert(annotation.into(), annotation_msg.into());
+        annotation_order.push(annotation.into());
+    }
+
+    // Next we get all the clauses.
+    let clause_names = ["args", "in", "stdout", "stderr", "exitcode"];
     let mut clauses = Vec::with_capacity(clause_names.len());
     for clause in clause_names.iter() {
-        if bless && (clause == &"stdout" || clause == &"stderr") {
+        let is_blessable_clause = can_be_blessed(clause);
+        if bless && is_blessable_clause {
             // These will get updated later, so just make them empty for now.
             clauses.push("".into());
             continue;
@@ -81,6 +116,7 @@ fn mk_test_case(mut content: String, bless: bool) -> Result<TestCase, Outcome> {
                 content = splits.pop().unwrap();
                 continue;
             }
+            let test_name = test_name.to_owned();
             return Err(Outcome::Failed {
                 msg: Some(atomic_lock(move |printer: &mut dyn LinePrinter| {
                     printer.print_line(
@@ -88,31 +124,46 @@ fn mk_test_case(mut content: String, bless: bool) -> Result<TestCase, Outcome> {
                         &LineFormat::Failure,
                     );
                     printer.print_line(
-                        &format!(
-                            r#"Hint: add a
+                        format!(
+                            r#"
+Hint: Add a
 
-{clause_delim}
-<text>
-{clause_delim}
+      {clause_delim}
+      <text>
+      {clause_delim}
 
-section to the test file."#,
+      section to the test file.
+"#,
                             clause_delim = clause_delim
-                        ),
+                        )
+                        .trim(),
                         &LineFormat::Suggestion,
                     );
+                    if is_blessable_clause {
+                        printer.print_line(
+                            &format!(
+                                "Hint: You can run `{}` to do this for you.",
+                                get_bless_cmd(&test_name)
+                            ),
+                            &LineFormat::Suggestion,
+                        );
+                    }
                 })),
             });
         }
-        content = splits.pop().unwrap();
-        clauses.push(splits.pop().unwrap());
+        content = splits.pop().unwrap(); // next content is the last split
+        clauses.push(splits.pop().unwrap()); // clause content is the second split
     }
     let mut clauses = clauses.into_iter();
 
     Ok(TestCase {
+        annotations,
+        annotation_order,
         args: clauses.next().unwrap(),
         input: clauses.next().unwrap(),
         stdout: clauses.next().unwrap(),
         stderr: clauses.next().unwrap(),
+        exitcode: clauses.next().unwrap(),
     })
 }
 
@@ -131,8 +182,22 @@ fn print_diff(printer: &mut dyn LinePrinter, text1: &str, text2: &str) {
 }
 
 /// Generates the contents of a blessed file for a test case.
-fn mk_bless_file(test_case: &TestCase, stdout: &str, stderr: &str) -> String {
+fn mk_bless_file(test_case: &TestCase, stdout: &str, stderr: &str, exitcode: &str) -> String {
     let mut content = String::with_capacity(256);
+
+    if !test_case.annotations.is_empty() {
+        for annotation in test_case.annotation_order.iter() {
+            let msg = test_case.annotations.get(annotation).unwrap();
+            if !msg.is_empty() {
+                content.push_str(&format!("{}: {}\n", annotation, msg));
+            } else {
+                content.push_str(&annotation);
+                content.push('\n');
+            }
+        }
+        content.push('\n');
+    }
+
     let mut push = |clause: &str, clause_content: &str| {
         let clause_delim = get_clause_delim(clause);
         content.push_str(&format!("{}\n", clause_delim));
@@ -145,6 +210,7 @@ fn mk_bless_file(test_case: &TestCase, stdout: &str, stderr: &str) -> String {
     push("in", &test_case.input);
     push("stdout", stdout);
     push("stderr", stderr);
+    push("exitcode", exitcode);
     content.pop(); // drop trailing newline
     content
 }
@@ -157,16 +223,29 @@ fn atomic_lock<T>(obj: T) -> Arc<Mutex<T>> {
 /// Runs a slide system test.
 fn drive_test(test: &Test<PathBuf>) -> Outcome {
     let bless = env::var("BLESS") == Ok("1".into());
+    let fail_todo = env::var("FAIL_TODO") == Ok("1".into());
 
     let path = &test.data;
+    let test_name = test.name.to_string();
     let content = fs::read(path).unwrap_or_else(|_| panic!("Failed to read {}", path.display()));
     let content = String::from_utf8(content)
         .unwrap_or_else(|_| panic!("{} is not valid UTF-8", path.display()));
 
-    let test_case = match mk_test_case(content, bless) {
+    let test_case = match mk_test_case(&test_name, content, bless) {
         Ok(tc) => tc,
         Err(outcome) => return outcome,
     };
+
+    if fail_todo && test_case.annotations.contains_key("@TODO") {
+        return Outcome::Failed {
+            msg: Some(atomic_lock(|printer: &mut dyn LinePrinter| {
+                printer.print_line(
+                    "Test is marked @TODO, which the test runner is set to fail on.",
+                    &LineFormat::Failure,
+                );
+            })),
+        };
+    }
 
     let mut cmd = Command::new("cargo");
     cmd.arg("run");
@@ -180,6 +259,7 @@ fn drive_test(test: &Test<PathBuf>) -> Outcome {
             cmd.arg(sub_arg);
         }
     }
+    cmd.arg("--");
     cmd.arg(&test_case.input);
 
     let cmd = match cmd.output() {
@@ -195,9 +275,10 @@ fn drive_test(test: &Test<PathBuf>) -> Outcome {
 
     let stdout = String::from_utf8(cmd.stdout).unwrap();
     let stderr = String::from_utf8(cmd.stderr).unwrap();
+    let exitcode = cmd.status.code().unwrap().to_string() + "\n";
 
     if bless {
-        let blessed = mk_bless_file(&test_case, &stdout, &stderr);
+        let blessed = mk_bless_file(&test_case, &stdout, &stderr, &exitcode);
         return match fs::write(path, blessed) {
             Ok(_) => Outcome::Passed,
             Err(e) => Outcome::Failed {
@@ -208,7 +289,7 @@ fn drive_test(test: &Test<PathBuf>) -> Outcome {
         };
     }
 
-    if stdout != test_case.stdout || stderr != test_case.stderr {
+    if stdout != test_case.stdout || stderr != test_case.stderr || exitcode != test_case.exitcode {
         return Outcome::Failed {
             msg: Some(atomic_lock(move |printer: &mut dyn LinePrinter| {
                 if stdout != test_case.stdout {
@@ -219,6 +300,17 @@ fn drive_test(test: &Test<PathBuf>) -> Outcome {
                     printer.print_line("Mismatch in stderr:", &LineFormat::Text);
                     print_diff(printer, &test_case.stderr, &stderr);
                 }
+                if exitcode != test_case.exitcode {
+                    printer.print_line("Mismatch in exit code:", &LineFormat::Text);
+                    print_diff(printer, &test_case.exitcode, &exitcode);
+                }
+                printer.print_line(
+                    &format!(
+                        "Help: If this is expected, try running `{}`.",
+                        get_bless_cmd(&test_name),
+                    ),
+                    &LineFormat::Suggestion,
+                );
             })),
         };
     }

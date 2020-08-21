d7ceb7e8cf8bfcbbe24abf3212b65debee635cda
Ayaz Hafiz <ayaz.hafiz.1@gmail.com>
2020-08-20 22:52:37 -0700

    Slide web UI
    This commit adds a web UI for slide, continuing off @lukebhan's work in
     #156. The UI is static, using a wasm build of slide to run in the
    browser. The UI will be built continuously to http://yslide.github.io on
    each commit to master.
    
    To enable easier iteration during development, `ladder www` has been
    added as a command to spin up a local file server for the UI and watch
    changes to slide. In practice, this should be useful for development
    too.
    
    The UI supports changing all options slide currently supports and
    preserves state in the URL, enabling easy sharing.
    
    In later iterations we can add information on current commit/version,
    links to a bug report, etc.
    
    I highly recommend checking this branch out locally to review the UI. A
    screenshot is attached in the PR associated with this commit.
    
    Closes #115
    Closes #169

--- slide/fuzz/fuzz_targets/slide.rs
+++ slide/fuzz/fuzz_targets/slide.rs
@@ -1,14 +1,13 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use libslide::EmitFormat;
 use slide::{run_slide, Opts};
 
 fuzz_target!(|program: String| {
     run_slide(Opts {
         program,
-        emit_format: EmitFormat::Pretty,
+        emit_format: "pretty".to_string(),
         parse_only: true,
         expr_pat: false,
-        no_emit: true,
+        color: false,
     });
 });
